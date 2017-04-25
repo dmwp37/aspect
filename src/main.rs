d18429cd03821e82b5d4f7630eafd620938736f2
Xudong Huang <huangxu008@hotmail.com>
2017-04-25 10:43:36 +0800

    :art: add one more aspect

--- src/main.rs
+++ src/main.rs
@@ -4,13 +4,20 @@ pub struct SubField {
 }
 
 #[derive(Debug)]
+pub struct NextAspect {
+    data: String,
+}
+
+#[derive(Debug)]
 struct A {
     f: SubField,
+    n: NextAspect,
 }
 
 #[derive(Debug)]
 struct B {
     f: SubField,
+    n: NextAspect,
 }
 
 impl<'a> From<&'a A> for &'a SubField {
@@ -25,9 +32,26 @@ impl<'a> From<&'a B> for &'a SubField {
     }
 }
 
+impl<'a> From<&'a A> for &'a NextAspect {
+    fn from(origin: &'a A) -> Self {
+        &origin.n
+    }
+}
+
+impl<'a> From<&'a B> for &'a NextAspect {
+    fn from(origin: &'a B) -> Self {
+        &origin.n
+    }
+}
+
 pub trait SubTrait {
     fn sub_fn(self) -> u32;
 }
+
+pub trait NextAspectTrait<'a> {
+    fn get_next(self) -> &'a str;
+}
+
 
 impl<'a, T> SubTrait for T
     where T: Into<&'a SubField>
@@ -37,13 +61,28 @@ impl<'a, T> SubTrait for T
     }
 }
 
-
+impl<'a, T> NextAspectTrait<'a> for T
+    where T: Into<&'a NextAspect>
+{
+    fn get_next(self) -> &'a str {
+        &self.into().data
+    }
+}
 fn main() {
-    println!("Hello, world!");
-    let a = A { f: SubField { data: 10 } };
-    let b = B { f: SubField { data: 32 } };
+    let a = A {
+        f: SubField { data: 10 },
+        n: NextAspect { data: "hello".to_owned() },
+    };
+    let b = B {
+        f: SubField { data: 32 },
+        n: NextAspect { data: "world".to_owned() },
+    };
+
     let x = b.sub_fn();
     println!("x = {:?}", x);
+    let y = a.get_next();
+    println!("y = {:?}", y);
+
     println!("a.data = {}", a.sub_fn());
     println!("b.data = {}", b.sub_fn());
     println!("a = {:?}", a);
